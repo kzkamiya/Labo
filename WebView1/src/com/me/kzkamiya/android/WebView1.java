45a9fde07a27f222b6aa076e11d4ead418e87c91
kzkamiya <kzkamiya@me.com>
2011-06-18 20:54:24 +0900

    Change-Id: If5cee1ad8d17aaa3aac21cb00ce7d37dd3e08cb2
    Updated. WebView Sample

--- WebView1/src/com/me/kzkamiya/android/WebView1.java
+++ WebView1/src/com/me/kzkamiya/android/WebView1.java
@@ -1,13 +1,52 @@
 package com.me.kzkamiya.android;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class WebView1 extends Activity {
+	
+	WebView webView;
+	
+	final class JavaScriptInterface {
+		private static final String KEY_HIGH_SCORE = "HIGH_SCORE";
+
+		JavaScriptInterface() {}
+		
+		public int getScreenWidth() {
+			return webView.getWidth();
+		}
+		
+		public int getScreenHeight() {
+			// -5 pixcels to prevent vertical scrolling.
+			return webView.getHeight() - 5;
+		}
+		
+		public int getHighScore() {
+			SharedPreferences preferences = getPreferences(MODE_WORLD_WRITEABLE);
+			return preferences.getInt(KEY_HIGH_SCORE, 0);
+		}
+		
+		public void setHighScore(int value) {
+			SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+			Editor editor = preferences.edit();
+			editor.putInt(KEY_HIGH_SCORE, value);
+			editor.commit();
+		}
+	}
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        webView.addJavascriptInterface(new JavaScriptInterface(), "android");
+        webView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
+        setContentView(webView);
     }
 }
