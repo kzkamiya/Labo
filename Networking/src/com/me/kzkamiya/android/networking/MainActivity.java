70ec47bbd4b8907a5bbf8b38b8edeed86d11c1fd
kzkamiya <kzkamiya@me.com>
2011-07-05 21:49:08 +0900

    Change-Id: Ibf6245c56b0c09e898f57f39d684245d28051743

--- Networking/src/com/me/kzkamiya/android/networking/MainActivity.java
+++ Networking/src/com/me/kzkamiya/android/networking/MainActivity.java
@@ -1,22 +1,40 @@
 package com.me.kzkamiya.android.networking;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	ImageView img;
-	
+
+	private class BackgroudTask extends AsyncTask<String, Void, Bitmap> {
+		protected Bitmap doInBackground(String ... url) {
+			Bitmap bitmap = DownloadImage(url[0]);
+			return bitmap;
+		}
+		protected void onPostExecute(Bitmap bitmap) {
+			ImageView img = (ImageView)findViewById(R.id.img);
+			img.setImageBitmap(bitmap);
+		}
+	}
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,10 +42,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         
         //---download an image---
-        Bitmap bitmap = 
-        	DownloadImage("http://211.10.14.129/img/cu_logo01.gif");
-        img = (ImageView) findViewById(R.id.img);
-        img.setImageBitmap(bitmap);
+//        new BackgroudTask().execute("http://211.10.14.129/img/cu_logo01.gif");
+//        Bitmap bitmap = 
+//        	DownloadImage("http://211.10.14.129/img/cu_logo01.gif");
+//        img = (ImageView) findViewById(R.id.img);
+//        img.setImageBitmap(bitmap);
+        
+        DownladFileSave("http://211.10.14.129/img/cu_logo01.gif");
+        
+        
+        final Context context = this; 
+        FileInputStream fileInputStream;
+		try {
+			fileInputStream = 
+				context.openFileInput("test.jpg");
+			Bitmap bitmap = BitmapFactory.decodeStream(fileInputStream);
+		    img = (ImageView) findViewById(R.id.img);
+		    img.setImageBitmap(bitmap);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
     }
     
     private InputStream OpenHttpConnection(String urlString) throws IOException 
@@ -69,5 +106,38 @@ public class MainActivity extends Activity {
     	}
     	return bitmap;
     }
+    
+	private boolean DownladFileSave(String URL) {
+		byte[] buffer = new byte[8 * 1024];
+		String filePath = "/data/data/" + this.getPackageName() + "/test.jpg";
+		InputStream input = null;
+		try {
+			input = OpenHttpConnection(URL);
+			FileOutputStream output = openFileOutput("test.jpg", Context.MODE_APPEND | Context.MODE_PRIVATE);
+//new FileOutputStream(new File(filePath));
+			try {
+				int bytesRead;
+				while ((bytesRead = input.read(buffer)) != -1) {
+					output.write(buffer, 0, bytesRead);
+				}
+			} finally {
+				output.close();
+			}
+		} catch (FileNotFoundException e) {
+			Log.getStackTraceString(e);
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.getStackTraceString(e);
+			e.printStackTrace();
+		} finally {
+			try {
+				input.close();
+			} catch (IOException e) {
+				Log.getStackTraceString(e);
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
     
 }
