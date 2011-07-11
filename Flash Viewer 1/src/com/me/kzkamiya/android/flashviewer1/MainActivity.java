6b44624760006136dd836c28a2d77d9423e01b24
kzkamiya <kzkamiya@me.com>
2011-07-11 11:08:22 +0900

    Added new activities.
    Change-Id: Id4287ed1b16a584032a74695ea6ede79f8fee6c4

--- Flash Viewer 1/src/com/me/kzkamiya/android/flashviewer1/MainActivity.java
+++ Flash Viewer 1/src/com/me/kzkamiya/android/flashviewer1/MainActivity.java
@@ -8,13 +8,52 @@ import com.me.kzkamiya.android.download.DownloadBackgroundTask;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+	private static final String TAG = MainActivity.class.getSimpleName();
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		
+		Log.d(TAG,"onDestroy() called!");
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d(TAG,"onPause() called!");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d(TAG,"onResume() called!");
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.d(TAG,"onStart() called!");
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d(TAG,"onStop() called!");
+	}
+
 	ImageView img;
 	
     /** Called when the activity is first created. */
@@ -26,8 +65,8 @@ public class MainActivity extends Activity {
         File saveFile = this.getFileStreamPath("esz.zip");
         String saveFilePath = saveFile.getAbsolutePath();
 
-        DownloadBackgroundTask downloadTask = new DownloadBackgroundTask();
-        downloadTask.execute("http://10.255.250.68/esz.zip", saveFilePath);
+//        DownloadBackgroundTask downloadTask = new DownloadBackgroundTask();
+//        downloadTask.execute("http://10.255.250.68/esz.zip", saveFilePath);
 
         
 //		try {
@@ -38,5 +77,25 @@ public class MainActivity extends Activity {
 //			Log.getStackTraceString(e);
 //			e.printStackTrace();
 //		}
+		Log.d(TAG,"onDestroy() called!");
+   }
+    
+    
+    public void onNavButtonClick(View view) {
+    	Intent intent = null;
+    	switch (view.getId()) {
+		case R.id.btn_sensors:
+			intent = new Intent(this, MainActivity.class);
+			break;
+		case R.id.btn_staff:
+			intent = new Intent(this, Staff.class);
+			break;
+		case R.id.btn_starlog:
+			intent = new Intent(this, StarLog.class);
+			break;
+		}
+    	startActivity(intent);
     }
+    
+    
 }
---
+++ Flash Viewer 1/src/com/me/kzkamiya/android/flashviewer1/StarLog.java
@@ -0,0 +1,33 @@
+package com.me.kzkamiya.android.flashviewer1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StarLog extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.starlog);
+	}
+
+    public void onNavButtonClick(View view) {
+    	Intent intent = null;
+    	switch (view.getId()) {
+		case R.id.btn_sensors:
+			intent = new Intent(this, MainActivity.class);
+			break;
+		case R.id.btn_staff:
+			intent = new Intent(this, Staff.class);
+			break;
+		case R.id.btn_starlog:
+			intent = new Intent(this, StarLog.class);
+			break;
+		}
+    	startActivity(intent);
+    }
+}
